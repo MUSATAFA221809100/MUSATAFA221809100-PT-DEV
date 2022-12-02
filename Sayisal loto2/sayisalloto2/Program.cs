344df94a1eaabae5dfa0d0cd019a0b267ba0f0f5
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-02 23:11:27 +0300

    Add files via upload

---
+++ Sayisal loto2/sayisalloto2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sayisalloto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] rastgele = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                rastgele[i] = rnd.Next(2, 49);
+            }
+
+            Array.Sort(rastgele);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(rastgele[i] + "");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
