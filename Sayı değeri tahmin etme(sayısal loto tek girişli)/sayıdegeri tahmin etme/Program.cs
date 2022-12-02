86110881a827f02f72b163b71fe34ad98e790fe7
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-02 22:57:33 +0300

    Add files via upload
    SAYI DEĞERLERİ TAHMİN ETME SAYISAL LOTO MANTIĞINDA TEK SAYI GİRMELİ

---
+++ Sayı değeri tahmin etme(sayısal loto tek girişli)/sayıdegeri tahmin etme/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sayıdegeri_tahmin_etme
+{
+    class Program
+
+    /*  sayı tahmin ettme programıdır tek sayi girebilirsiniz ve sistem kendi içindeki algoritamsından  seçer ver doğru veya yanlış bildiniz diye bilgilendirir ve doğru cevaplancıya kadar çalıışır ve devam ettirebilinir */
+
+
+    {
+        static void Main(string[] args)
+        { 
+           Random _rnd = new Random();
+        int _uretilenSayi = _rnd.Next(10, 21);
+
+            Console.WriteLine("bir sayi tahmin ediniz..");
+            while(true)
+            {
+                int _tahminEdilenSayi = Convert.ToInt32(Console.ReadLine());
+         
+            if(_tahminEdilenSayi == _uretilenSayi)
+        
+            {
+                Console.WriteLine("Tebrikler doğru tahmin ettiniz..");
+
+                  break;
+            }    
+               
+                else 
+                {
+                        Console.WriteLine("yanlış tahminde bulundunuz . Lütfen tekrar deneyiniz....");
+              
+                
+                }
+              
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
