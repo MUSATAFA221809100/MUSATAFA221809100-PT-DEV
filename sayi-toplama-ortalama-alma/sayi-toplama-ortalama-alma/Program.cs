8621a7c3904144a34efe92a5449989f6d11d2115
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-12 13:15:32 +0300

    Add files via upload

---
+++ sayi-toplama-ortalama-alma/sayi-toplama-ortalama-alma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sayi_toplama_ortalama_alma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double toplam = 0, ortalama = 0;
+                Console.Write("Kaç elemanlı dizi oluşturulsun :");// Ekrana yazacak en başta kaç elemanlı dizi oluşturulsun diye [5 elemanlı olarak]
+                int n = Int32.Parse(Console.ReadLine());
+                double[] dizi = new double[n];
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    Console.Write("Sayı {0}: ", i + 1);
+                    dizi[i] = Convert.ToDouble(Console.ReadLine());
+                    toplam += dizi[i];
+                }
+                ortalama = toplam / dizi.Length; // Ekrana girilmiş olan sayıların toplayıp kaç adet girilmiş olan sayıya bölerek ortalamasını bulur ve orlama değişkenine atar
+                Console.WriteLine("Toplam : " + toplam); // Ekrana girilmiş olan sayıların toplamını yazar
+                Console.WriteLine("Ortalama : " + ortalama);// Ekrana girilmiş olan sayıların toplayıp kaç adet girilmiş olan sayıya bölerek ortalamasını ekrana yazar
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
