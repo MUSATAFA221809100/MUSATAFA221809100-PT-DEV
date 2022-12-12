a4f484c0afbe18722c6c15b22682dd42d263682f
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-12 13:16:07 +0300

    Add files via upload

---
+++ GirilenSayının_dizinin_elemanlarını_yüzde_20_arttırıp_e.y/GirilenSayının_dizinin_elemanlarını_yüzde_20_arttırıp_e.y/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GirilenSayının_dizinin_elemanlarını_yüzde_20_arttırıp_e.y_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Kullanıcı tarafından 10 elemanlı bir diziye sayı girişi yaptıran ve bu dizinin elemanlarını %20 arttırıp ekrana yazdıran For döngüsü örneği
+
+          
+            //Eleman Girme işlemi
+            int[] dizi = new int[10];
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.Write("{0}. elemanı giriniz : ", i + 1);
+                        dizi[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("=========================================");
+                  
+            // Yazdırma işlemi
+                    for (int i = 0; i < 10; i++)
+                    {
+                        dizi[i] = dizi[i] + (dizi[i] * 20 / 100);
+                        Console.WriteLine("Dizinin {0}. indexi : {1} ", i, dizi[i]);
+                    }
+                    Console.ReadKey();
+            
+           
+        }
+    }
+}
