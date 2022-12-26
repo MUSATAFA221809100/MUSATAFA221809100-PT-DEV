2ec23b4ded8fab1768918b0d24204a36b9c09f39
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-26 12:12:08 +0300

    Add files via upload

---
+++ Girilen Küçük Harfleri Büyük Harfe Döüştürme/Girilen Küçük Harfleri Büyük Harfe Döüştürme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Girilen_Küçük_Harfleri_Büyük_Harfe_Döüştürme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metin giriniz ( Türkçe karekter kullamayınız ): ");// ekrana metn girmemizi isteyecek türkçe karekter kulanmamız istemkteyimm
+            string metin = Console.ReadLine();
+
+            for ( int i=0; i<metin.Legenth; i++) // dizimiz sıfırıncı indisten  başalyacak metinin uzunluğu kadar çalışacak.
+            {
+                int a = metin[i]; //metindeki aski kodlar ile yaılacak
+                int b = a - 32; // her karekteriçin aynı döngüden ilerler taki metin.legent döndürülecek karekter bitti deinceye kadar
+                Console.Write((char)b);
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
