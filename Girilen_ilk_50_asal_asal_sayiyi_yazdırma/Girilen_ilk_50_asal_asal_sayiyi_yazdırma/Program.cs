38a3ae678db9154d4f212e2faea55f028cd4104b
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-26 12:12:31 +0300

    Add files via upload

---
+++ Girilen_ilk_50_asal_asal_sayiyi_yazdırma/Girilen_ilk_50_asal_asal_sayiyi_yazdırma/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen_ilk_50_asal_asal_sayiyi_yazdırma
+{
+    class Program
+    {
+        //girilen sayıların ilk 50 asal sayı 10′ arlı olarak ekranda yazdırma
+        static void Main(string[] args)
+        {
+            int sayi = 2; // sayı değişknine 2 verdik  çükü asal sayılar alailmemiz için
+            int sayac = 0; //sayaç deişkeni 0 vedik
+            int sayac2 = 0; //sayaç 2 deişkeni 0 vedik
+            int sayac3 = 0; //sayaç3 deişkeni 0 vedik
+            Console.WriteLine("İlk 50 Asal Sayı");     //ekrana    İlk 50 Asal Sayı  yazar
+            while (true)
+            {
+
+                sayac = 0;
+                if (sayac2 == 50)
+                {
+                    break; //sayi değişeni birer birer  arttır
+                }
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        sayac++;//sayaç değişeni birer birer  arttır
+                        break; // bitikten sonra döngüyü bırak
+                    }
+
+                }
+                if (sayac == 0)
+                {
+                    sayac3++;
+                    Console.Write(sayi + "\t");
+                    if (sayac3 % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                    sayac2++;
+                }
+                sayi++; //sayi değişeni birer birer  arttır
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
