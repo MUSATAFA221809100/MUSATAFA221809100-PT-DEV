d5f663521e8fccd13045928ca7042027b4130e13
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2022-12-27 15:02:31 +0300

    Add files via upload

---
+++ Girilen_Sayinin_Karesini_Alma/Girilen_Sayinin_Karesini_Alma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Girilen_Sayinin_Karesini_Alma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı  : ");//ekrana sayi: yazdırıyoruz  5 girdiimizde
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sonuc = sayi * sayi; // burda sayinin karesini alma işlemini son deikeie atadık aynı sayıyı iki kez çarpmakdır karesi almak
+            Console.WriteLine(sayi + " sayısının karesi : " + sonuc); //ekrana sayinin karesi alınmıs ekilde yazacaktir   25 olaca
+
+            Console.ReadKey();
+        }
+    }
+}
